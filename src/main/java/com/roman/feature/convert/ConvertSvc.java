8d5703137229f990ebff6f1d5d6afb17281f7bd5
NV <NVardell@GMail.com>
2023-11-09 13:45:05 -0500

    convert controller service interface

---
+++ src/main/java/com/roman/feature/convert/ConvertSvc.java
@@ -0,0 +1,12 @@
+package com.roman.feature.convert;
+
+/**
+ * Convert Service Interface
+ *
+ * @author NV
+ * @since 11/9/2023
+ */
+public interface ConvertSvc {
+    String getRoman(String x);
+    String getInteger(String x);
+}

c43f1a566c0f6148a3eb5abd3956af2be1398ccb
NV <NVardell@GMail.com>
2023-11-09 13:49:02 -0500

    added static final arrays for integer roman numeral representations

--- src/main/java/com/roman/feature/convert/ConvertSvcImpl.java
+++ src/main/java/com/roman/feature/convert/ConvertSvcImpl.java
@@ -14,6 +14,52 @@ import static java.lang.Integer.parseInt;
 @Log4j2
 @Service public class ConvertSvcImpl implements ConvertSvc {
 
+        // Single Digit Roman Numeral Representations
+    private static final String[] ones = { "",
+            "I",     // 1
+            "II",    // 2
+            "III",   // 3
+            "IV",    // 4
+            "V",     // 5
+            "VI",    // 6
+            "VII",   // 7
+            "VIII",  // 8
+            "IX"     // 9
+    };
+
+    // Double Digit Roman Numeral Representations
+    private static final String[] tens = { "",
+            "X",     // 10
+            "XX",    // 20
+            "XXX",   // 30
+            "XL",    // 40
+            "L",     // 50
+            "LX",    // 60
+            "LXX",   // 70
+            "LXXX",  // 80
+            "XC"     // 90
+    };
+
+    // Triple Digit Roman Numeral Representations
+    private static final String[] hundreds = { "",
+            "C",     // 100
+            "CC",    // 200
+            "CCC",   // 300
+            "CD",    // 400
+            "D",     // 500
+            "DC",    // 600
+            "DCC",   // 700
+            "DCCC",  // 800
+            "CM"     // 900
+    };
+
+    // Quadruple Digit Roman Numeral Representations
+    private static final String[] thousands = { "",
+            "M",     // 1000
+            "MM",    // 2000
+            "MMM"    // 3000
+    };
+
     public String getRoman(String x) {
         log.info("ConvertSvc || Generating roman numeral for integer={}", x);
         return null;
